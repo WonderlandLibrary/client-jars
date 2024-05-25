01a213b27c44b7a594195047d5d4fc01acea1d62
Párko-pes <144590047+jezevcik@users.noreply.github.com>
2024-05-25 14:16:32 +0200

    Added available list

--- generate-readme.py
+++ generate-readme.py
@@ -5,11 +5,17 @@ template = open("README.template", "r").read()
 output = template
 
 jars_out = ""
+available_out = ""
 for jar_name in os.listdir("jars"):
     jars_out += f"- {jar_name}\n"
+    available_out += f"{jar_name}\n"
 
 output = output.replace("jars-here", jars_out)    
 
 f = open("README.md", "w")
 f.write(output)
 f.close()
+
+available = open("available", "w")
+available.write(available_out)
+available.close()
